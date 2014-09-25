e72c4f52c3d61701257a78261d4bddb72d48ca46
mxml <mxml@localhost.localdomain>
2014-09-25 18:57:34 +0200

    * now creating a surface for pdf or svg depending on output name extension \n* new functions to clip a circle, square and SineWave but latter isn't working correctly

--- cairobou.py
+++ cairobou.py
@@ -2,15 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os.path
 import math
 import cairo
 import optparse
 import logging
-
-
-# globale variables
-g_options = False # command line options
-g_images = False # images to process
 
 # constants
 INCH = 72 # 72 pixels (p) = 1 inch (in)
@@ -18,23 +14,50 @@ MM = INCH / 25.4 # 25.4 milimeters (mm) = 1 in
 CM = INCH / 2.54 # 2.54 centimetes (cm) = 1 in
 A4_WIDTH, A4_HEIGHT = INCH * 8.3, INCH * 11.7 # DIN A4 Paper is 297mm heigh and 210mm wide
 
-def circle():
-	image = cairo.ImageSurface.create_from_png(g_images[0])
+def clipCircle(context, cx, cy, r):
+	context.arc(cx, cy, r, 0, 2*math.pi)
+	context.clip()
+
+def clipCube(context, cx, cy, a):
+	context.rectangle(cx-(a/2), cy-(a/2), a, a)
+	context.clip()
+
+def clipSineWave(context, cx, cy , amplitude, width):
+	start = cx - width/2
+	end = cx + width/2
+
+	context.move_to(start, cy)
+	context.curve_to(cx-width/4,cy+amplitude, cx, cy, cx+width/4, cy-amplitude)
+	context.set_line_width(30)
+	context.stroke()
+	context.clip()
+
+def doIt(output, images):
+	image = cairo.ImageSurface.create_from_png(images[0])
 	width = cairo.ImageSurface.get_width(image)
 	height = cairo.ImageSurface.get_height(image)
 	
 	size = max(width, height)
-	surface = cairo.PDFSurface(g_options.out, size, size)
+	extension = os.path.splitext(output)[1]
+	surface = False
+	if(extension == ".pdf"):
+		surface = cairo.PDFSurface(output, size, size)
+	elif(extension == ".svg"):
+		surface = cairo.SVGSurface(output, size, size)
+
 	cr = cairo.Context(surface)
 
-	imageCount = len(g_images)
+	imageCount = len(images)
 	radius = min(width, height)/2
 	d = 30
 	loop = 0
+
 	while radius > 50:
-		image = cairo.ImageSurface.create_from_png(g_images[loop % imageCount])
-		cr.arc(size/2, size/2, radius, 0, 2*math.pi)
-		cr.clip()
+		image = cairo.ImageSurface.create_from_png(images[loop % imageCount])
+	
+		#clipSineWave(cr, size/2, size/2,height/2,width)
+		clipCircle(cr, size/2, size/2, radius)
+
 		cr.set_source_surface(image, 0, height/4)
 		cr.paint()
 
@@ -46,35 +69,40 @@ def main():
 	usage = "usage: %prog [options] images*"
 	parser = optparse.OptionParser(usage = usage)
 	parser.add_option("-o", "--out", dest="out", type="string",
-					help="specify output file", default="cairobou.pdf")
-
+					help="specify output file", default="output.svg")
 	parser.add_option("-v", "--verbose", action="store_true",
 					help="print status messages to stdout", default=False)
 	parser.add_option("-d", "--debug", action="store_true",
 					help="print status and debug messages to stdout", default=False)
-	global g_options
-	global g_images
-	(g_options, g_images) = parser.parse_args()
+
+	(options, images) = parser.parse_args()
 
 	# checking if enough images are specified
-	if(len(g_images)<1):
+	if(len(images)<1):
 		parser.error("At least one image has to be specified!")
 		return 1
 
+	# checking if output format is supported
+	extension = os.path.splitext(options.out)[1]
+	supported = [".pdf",".svg"]
+	if(extension not in [".pdf",".svg"]):
+		parser.error("Unsupported output format. Currently supported: "+", ".join(supported))
+		return 1
+
 	# defining output
-	if(g_options.debug):
+	if(options.debug):
 		logging.basicConfig(format='%(message)s', level="DEBUG")
 		# Printing Options for Debugging
 		for option in parser.option_list:
 			if(option.dest != None):
-				logging.debug("%s = %s", option, getattr(g_options, option.dest))
-		logging.debug("Processing %d image(s):", len(g_images))
-		for image in g_images:
+				logging.debug("%s = %s", option, getattr(options, option.dest))
+		logging.debug("Processing %d image(s):", len(images))
+		for image in images:
 			logging.debug("\t%s",str(image))
-	elif(g_options.verbose):
+	elif(options.verbose):
 		logging.basicConfig(format='%(message)s', level="INFO")
-
-	circle()
+	
+	doIt(options.out, images)
 
 	return 0
 
